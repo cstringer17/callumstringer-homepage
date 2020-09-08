a64f0c084871d004d13fc29be428588e284641cf
Callum Stringer <callum.stringer@idorsia.com>
2020-09-08 15:46:40 +0200

    :tada: first commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import router from './router'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faHome,
+  faInfoCircle
+} from "@fortawesome/free-solid-svg-icons";
+import {
 
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(
+  faGithub,
+  faHome,
+  faInfoCircle
+);
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
-
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
